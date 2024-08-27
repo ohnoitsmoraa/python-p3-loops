603a54e96ce9267a6a767c79b8273c521724e057
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-08-27 15:48:15 +0300

    Modified looping.py to accept while loop

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+    i = 10
+    while i>=0:
+        print(i)
+        i -= 1
+        print("Happy New Year!")
+ 
     # code goes here!
     pass
 
 def square_integers(int_list):
+  for number in  int_list:
+      print(number ** 2)
+      return [number **2 for number in int_list]
+
     # code goes here!
-    pass
+    
 
 def fizzbuzz():
+     i = 0
+     while (i < 100):
+        i += 1
+        if (i % 3 == 0 and i % 5 ==0):
+             print("FizzBuzz")
+        elif(i % 3 == 0):
+             print("Fizz")
+        elif(i % 5 == 0):
+             print("Buzz")
+        else:
+             print(i)
+
     # code goes here!
-    pass
